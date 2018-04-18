2ae8ed7e218740c2c21aa9b7036df124359a9e14
leobaltazor <leo.baltazor@gmail.com>
2018-04-18 22:56:03 +0300

    dz ok

---
+++ index.js
@@ -0,0 +1,8 @@
+var text;
+
+document.querySelector('input.inp').addEventListener("change", val)
+
+function val() {
+	text = document.querySelector('input.inp').value
+	document.querySelector('.text').innerHTML = text
+}
